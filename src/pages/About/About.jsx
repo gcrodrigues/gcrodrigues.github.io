ec0c1eebe961d0b183c86e934c9519e29647350e
Gustavo <gcrodrigues413@gmail.com>
2020-08-15 19:47:25 -0300

    About page development

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Navbar } from "../../components";
+
+import { Container, Section } from "../../styles/Components";
+
+function About() {
+  return (
+    <Container>
+      <Navbar />
+      <Section></Section>
+    </Container>
+  );
+}
+
+export default About;
