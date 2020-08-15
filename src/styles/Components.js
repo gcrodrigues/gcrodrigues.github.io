1ea4f9e8dce8a7259929677ea36bd7947eb03bf6
Gustavo <gcrodrigues413@gmail.com>
2020-08-15 19:06:25 -0300

    Global component style

---
+++ src/styles/Components.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  overflow: ${({ hidden }) => (hidden ? "hidden" : "unset")};
+
+  @media (max-width: 770px) {
+    align-items: flex-end;
+    justify-content: center;
+  }
+`;
+
+export const Section = styled.section`
+  width: calc(100% - 5rem);
+  height: 100%;
+  @media (max-width: 770px) {
+    width: 100%;
+    height: calc(100% - 5rem);
+  }
+`;
