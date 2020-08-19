07673cb48c7c9d2a72c6675690a311682f23ff4b
Gustavo <gcrodrigues413@gmail.com>
2020-08-19 06:52:12 -0300

    About page development

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-import { Navbar } from "../../components";
+import { Medias, Navbar } from "../../components";
 
 import { Container, Section } from "../../styles/Components";
+import { Content } from "./styles";
 
 function About() {
   return (
     <Container>
       <Navbar />
-      <Section></Section>
+      <Section>
+        <Content>
+          <div>
+            <h2>Page under development!</h2>
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/1f4wL263wM4zzeECxngxDZUD4X1PYP01P/view?usp=sharing"
+            >
+              Download my resume
+            </a>
+            <p>Check my social medias</p>
+            <Medias />
+          </div>
+        </Content>
+      </Section>
     </Container>
   );
 }
---
+++ src/pages/About/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    background: ${(props) => props.theme.colors.complement};
+    padding: 2rem;
+    border-radius: 0.5rem;
+
+    > a {
+      padding: 1rem 2.5rem;
+      font-size: 1.6rem;
+      color: white;
+      text-decoration: none;
+      background-color: ${(props) => props.theme.colors.secundary};
+      margin: 1rem 0;
+    }
+
+    h2 {
+      font-size: 2.4rem;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
+
+    ul {
+      a {
+        color: ${(props) => props.theme.colors.text};
+        margin-top: 0;
+      }
+    }
+  }
+`;
