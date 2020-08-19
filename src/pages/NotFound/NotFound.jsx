f6eec2961a90e29bb06dbfb381a2a7e2e35f7268
Gustavo <gcrodrigues413@gmail.com>
2020-08-19 06:51:23 -0300

    Not found page development

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { ThemeContext } from "styled-components";
 import { Link } from "react-router-dom";
+
+import NotFoundDark from "../../assets/404dark.png";
+import NotFoundLight from "../../assets/404light.png";
+
 import styles from "./NotFound.module.css";
 
 function NotFound() {
+  const { title } = useContext(ThemeContext);
   return (
     <div className={styles.container}>
-      <h1>Sorry :(</h1>
-      <p>404 - Page not found</p>
+      <img src={title === "dark" ? NotFoundDark : NotFoundLight} alt="404" />
+
+      <h1>Sorry, page not found :(</h1>
       <Link to="/">Back to home page</Link>
     </div>
   );
