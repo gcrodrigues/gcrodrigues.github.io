30d8d2b63cbab88ff46c6ba15704336d6b19e34a
Gustavo <gcrodrigues413@gmail.com>
2020-08-19 06:48:09 -0300

    Fetch pinned repos with graphql

--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Repo } from "../";
+import api, { GET_PINNED_REPOS } from "../../services/api";
 
 import { Container } from "./styles.js";
 
-const ReposList = ({ repositories }) => {
+const ReposList = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.post("", { query: GET_PINNED_REPOS }).then(
+      ({
+        data: {
+          data: { user },
+        },
+      }) => {
+        const res = user.pinnedItems.nodes;
+        setRepos(res);
+      }
+    );
+  }, []);
+
   return (
     <Container>
-      {repositories.map((repo) => (
+      {repos.map((repo) => (
         <Repo
           key={repo.id}
           name={repo.name}
           url={repo.url}
+          homepage={repo.homepageUrl}
           description={repo.description}
         />
       ))}
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,28 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.github.com/users",
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
 });
+
+export const GET_PINNED_REPOS = `
+  {
+    user(login: "gcrodrigues") {
+      pinnedItems(first: 6, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            id
+            name
+            description
+            homepageUrl
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default api;
