2b69eabc68c92761b0a195c3d628d62372ad87b0
Gustavo Carvalho <gcrodrigues413@gmail.com>
2020-09-19 14:25:55 -0300

    Setting baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,28 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.github.com/graphql",
-  headers: {
-    Authorization: "bearer b015824201eaffcb7060ff53a6ca2c744f9340b4",
-  },
+  baseURL: "https://api.github.com/",
+
 });
 
-export const GET_PINNED_REPOS = `
-  {
-    user(login: "gcrodrigues") {
-      pinnedItems(first: 6, types: REPOSITORY) {
-        nodes {
-          ... on Repository {
-            id
-            name
-            description
-            homepageUrl
-            url
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default api;
