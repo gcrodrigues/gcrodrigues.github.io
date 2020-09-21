b993318f35078cd5ca040835d36bb3466844f73f
Gustavo Carvalho <gcrodrigues413@gmail.com>
2020-09-21 17:20:25 -0300

    Setting navbar for mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,123 +1,166 @@
 import React, { useContext } from "react";
 import { MdFolderSpecial, MdChat } from "react-icons/md";
+import { FiMenu, FiX } from "react-icons/fi";
 import { FaFileCode, FaHome, FaCloudMoon, FaSun } from "react-icons/fa";
 import ReactTooltip from "react-tooltip";
 import { ThemeContext } from "styled-components";
-import { Container, List, Anchor } from "./styles";
+import { Container, Nav, Anchor } from "./styles";
 import LayoutContext from "../../context/themeContext";
 
 const Navbar = () => {
-  const { toggleTheme, theme, width } = useContext(LayoutContext);
+  const { handleNavbar, toggleTheme, theme, width, navIsOpen } = useContext(
+    LayoutContext
+  );
   const { colors } = useContext(ThemeContext);
 
   return (
     <>
-      <Container>
-        <List>
-          <li>
-            <h1>
-              <span>GC</span>
-              <br />
-              Rodrigues
-            </h1>
-          </li>
-          <li data-for="home" data-tip>
+      <Container isOpen={navIsOpen}>
+        <button onClick={handleNavbar}>
+          {navIsOpen ? <FiX size={30} /> : <FiMenu size={30} />}
+        </button>
+
+        <h1>
+          <span>GC</span>
+          <br />
+          Rodrigues
+        </h1>
+        <div>
+          <Nav>
             <Anchor
+              onClick={handleNavbar}
+              data-for="home"
+              data-tip
               exact
               to="/"
-              activeStyle={{
-                color: colors.secundary,
-                backgroundColor: colors.background,
-              }}
+              activeStyle={
+                width > 770
+                  ? {
+                      color: colors.secundary,
+                      backgroundColor: colors.background,
+                    }
+                  : { backgroundColor: colors.active }
+              }
             >
-              <FaHome size={30} />
+              {width >= 770 ? <FaHome size={30} /> : <p>Home</p>}
             </Anchor>
-          </li>
-          <ReactTooltip
-            id="home"
-            place={width <= 770 ? "bottom" : "right"}
-            border={true}
-            textColor={colors.text}
-            borderColor={colors.secundary}
-            backgroundColor={colors.background}
-            effect="solid"
-          >
-            <p style={{ fontWeight: 600, fontSize: 14 }}>Home</p>
-          </ReactTooltip>
-          <li data-for="about" data-tip>
-            <Anchor
-              to="/about"
-              activeStyle={{
-                color: colors.secundary,
-                backgroundColor: colors.background,
-              }}
-            >
-              <FaFileCode size={30} />
-            </Anchor>
-          </li>
-          <ReactTooltip
-            id="about"
-            place={width <= 770 ? "bottom" : "right"}
-            effect="solid"
-            border={true}
-            textColor={colors.text}
-            borderColor={colors.secundary}
-            backgroundColor={colors.background}
-          >
-            <p style={{ fontWeight: 600, fontSize: 14 }}>About</p>
-          </ReactTooltip>
-          <li data-for="portfolio" data-tip>
-            <Anchor
-              to="/portfolio"
-              activeStyle={{
-                color: colors.secundary,
-                backgroundColor: colors.background,
-              }}
-            >
-              <MdFolderSpecial size={30} />
-            </Anchor>
-          </li>
-          <ReactTooltip
-            id="portfolio"
-            place={width <= 770 ? "bottom" : "right"}
-            effect="solid"
-            border={true}
-            textColor={colors.text}
-            borderColor={colors.secundary}
-            backgroundColor={colors.background}
-          >
-            <p style={{ fontWeight: 600, fontSize: 14 }}>Portfolio</p>
-          </ReactTooltip>
-          <li data-for="contact" data-tip>
-            <Anchor
-              to="/contact"
-              activeStyle={{
-                color: colors.secundary,
-                backgroundColor: colors.background,
-              }}
-            >
-              <MdChat size={30} />
-            </Anchor>
-          </li>
-          <ReactTooltip
-            id="contact"
-            place={width <= 770 ? "bottom" : "right"}
-            effect="solid"
-            border={true}
-            textColor={colors.text}
-            borderColor={colors.secundary}
-            backgroundColor={colors.background}
-          >
-            <p style={{ fontWeight: 600, fontSize: 14 }}>Contact</p>
-          </ReactTooltip>
-          <li onClick={toggleTheme}>
-            {theme.title === "dark" ? (
-              <FaCloudMoon size={30} />
-            ) : (
-              <FaSun size={30} />
+
+            {width > 770 && (
+              <ReactTooltip
+                id="home"
+                place="right"
+                border={true}
+                textColor={colors.text}
+                borderColor={colors.secundary}
+                backgroundColor={colors.background}
+                effect="solid"
+              >
+                <p style={{ fontWeight: 600, fontSize: 14 }}>Home</p>
+              </ReactTooltip>
             )}
-          </li>
-        </List>
+
+            <Anchor
+              onClick={handleNavbar}
+              data-for="about"
+              data-tip
+              to="/about"
+              activeStyle={
+                width > 770
+                  ? {
+                      color: colors.secundary,
+                      backgroundColor: colors.background,
+                    }
+                  : { backgroundColor: colors.active }
+              }
+            >
+              {width >= 770 ? <FaFileCode size={30} /> : <p>About</p>}
+            </Anchor>
+
+            {width > 770 && (
+              <ReactTooltip
+                id="about"
+                place="right"
+                effect="solid"
+                border={true}
+                textColor={colors.text}
+                borderColor={colors.secundary}
+                backgroundColor={colors.background}
+              >
+                <p style={{ fontWeight: 600, fontSize: 14 }}>About</p>
+              </ReactTooltip>
+            )}
+
+            <Anchor
+              onClick={handleNavbar}
+              data-for="portfolio"
+              data-tip
+              to="/portfolio"
+              activeStyle={
+                width > 770
+                  ? {
+                      color: colors.secundary,
+                      backgroundColor: colors.background,
+                    }
+                  : { backgroundColor: colors.active }
+              }
+            >
+              {width >= 770 ? <MdFolderSpecial size={30} /> : <p>Portfolio</p>}
+            </Anchor>
+
+            {width > 770 && (
+              <ReactTooltip
+                id="portfolio"
+                place="right"
+                effect="solid"
+                border={true}
+                textColor={colors.text}
+                borderColor={colors.secundary}
+                backgroundColor={colors.background}
+              >
+                <p style={{ fontWeight: 600, fontSize: 14 }}>Portfolio</p>
+              </ReactTooltip>
+            )}
+
+            <Anchor
+              onClick={handleNavbar}
+              data-for="contact"
+              data-tip
+              to="/contact"
+              activeStyle={
+                width > 770
+                  ? {
+                      color: colors.secundary,
+                      backgroundColor: colors.background,
+                    }
+                  : { backgroundColor: colors.active }
+              }
+            >
+              {width >= 770 ? <MdChat size={30} /> : <p>Contact</p>}
+            </Anchor>
+
+            {width > 770 && (
+              <ReactTooltip
+                id="contact"
+                place="right"
+                effect="solid"
+                border={true}
+                textColor={colors.text}
+                borderColor={colors.secundary}
+                backgroundColor={colors.background}
+              >
+                <p style={{ fontWeight: 600, fontSize: 14 }}>Contact</p>
+              </ReactTooltip>
+            )}
+          </Nav>
+        </div>
+
+        <button onClick={toggleTheme}>
+          {theme.title === "dark" ? (
+            <FaSun size={30} />
+          ) : (
+            <FaCloudMoon size={30} />
+          )}
+        </button>
       </Container>
     </>
   );
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-export const Container = styled.nav`
+export const Container = styled.header`
   position: fixed;
-  background-color: ${(props) => props.theme.colors.navbar};
   left: 0;
   top: 0;
-  z-index: 1;
+  z-index: 2;
+
   width: 100%;
   height: 8rem;
   padding: 0 1.5rem;
 
-  @media (min-width: 770px) and (max-height: 450px) {
+  background-color: ${(props) => props.theme.colors.navbar};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 770px) and (max-height: 421px) {
     width: calc(8rem + 0.72rem);
     overflow-y: scroll;
 
@@ -32,6 +38,14 @@ export const Container = styled.nav`
     width: 8rem;
     height: 100%;
     padding: 2rem 0;
+
+    flex-direction: column;
+  }
+
+  > button:first-child {
+    @media (min-width: 770px) {
+      display: none;
+    }
   }
 
   h1 {
@@ -48,38 +62,10 @@ export const Container = styled.nav`
       font-weight: 100;
     }
   }
-`;
 
-export const List = styled.ul`
-  display: flex;
-  list-style: none;
-  flex-direction: row;
-  justify-content: center;
-
-  @media (min-width: 770px) {
-    flex-direction: column;
-    height: 100%;
-    min-height: 450px;
-  }
-
-  li {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    justify-content: center;
-  }
-
-  li:first-child {
-    margin-bottom: 2rem;
-
-    @media (max-width: 770px) {
-      margin-bottom: 0;
-    }
-  }
-
-  li:last-child {
-    margin-top: auto;
-    padding: 1rem 0;
+  button {
+    border: 0;
+    background: transparent;
     cursor: pointer;
     color: ${(props) => props.theme.colors.icons};
     filter: brightness(100%);
@@ -88,12 +74,47 @@ export const List = styled.ul`
     &:hover {
       filter: brightness(80%);
     }
+  }
 
+  > div {
     @media (max-width: 770px) {
-      /* display: none; */
-      padding: 0;
-      margin-top: 0;
+      width: 100%;
+
+      position: absolute;
+      left: ${(props) => (props.isOpen ? "0" : "-100%")};
+      top: 7.5rem;
+
+      background-color: ${(props) => props.theme.colors.navbar};
+      padding: 1rem 1.5rem;
+
+      transition: left 0.2s;
+
+      z-index: 2;
+
+      &:after {
+        content: "";
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        left: 0;
+        top: 100%;
+        z-index: 1;
+        background-color: rgba(0, 0, 0, 0.6);
+        transition: opacity 1s ease-out;
+
+        opacity: ${(props) => (props.isOpen ? "1" : "0")};
+      }
     }
+  }
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  @media (min-width: 770px) {
+    margin: 1rem 0;
   }
 `;
 
@@ -101,7 +122,26 @@ export const Anchor = styled(NavLink)`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 8rem;
-  padding: 0 2.5rem;
+  height: 6rem;
+  padding: 0 1.5rem;
   color: ${(props) => props.theme.colors.icons};
+  text-decoration: none;
+
+  @media (max-width: 770px) {
+    padding: 0;
+    border-top: 0.1rem solid rgba(255, 255, 255, 0.1);
+  }
+
+  @media (min-width: 770px) {
+    border-radius: 50%;
+    &:not(:first-child) {
+      margin: 1.5rem 0 0;
+    }
+  }
+
+  p {
+    font-size: 1.4rem;
+    color: #fff;
+    font-weight: bold;
+  }
 `;
