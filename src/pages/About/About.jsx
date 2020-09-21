35ff3c8bcfd3e43e165265a9b993596eaab30828
Gustavo Carvalho <gcrodrigues413@gmail.com>
2020-09-21 17:24:00 -0300

    About page development

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,25 @@ import React from "react";
 import { Medias, Navbar } from "../../components";
 
 import { Container, Section } from "../../styles/Components";
-import { Content } from "./styles";
+import {
+  Content,
+  AboutCard,
+  ImageWrapper,
+  AboutMe,
+  Techs,
+  Tech,
+} from "./styles";
+import avatar from "../../assets/gustavo.jpg";
+import html from "../../assets/icons/html.svg";
+import css from "../../assets/icons/css.svg";
+import javascript from "../../assets/icons/javascript.svg";
+import typescript from "../../assets/icons/typescript.svg";
+import styled from "../../assets/icons/styled.svg";
+import react from "../../assets/icons/react.svg";
+import sass from "../../assets/icons/sass.svg";
+import redux from "../../assets/icons/redux.svg";
+import node from "../../assets/icons/node.svg";
+import git from "../../assets/icons/git.svg";
 
 function About() {
   return (
@@ -12,15 +30,77 @@ function About() {
       <Section>
         <Content>
           <div>
-            <h2>Page under development!</h2>
+            <AboutCard>
+              <ImageWrapper>
+                <img src={avatar} alt="Gustavo Carvalho avatar" />
+              </ImageWrapper>
+
+              <div>
+                <h1>Gustavo de Carvalho</h1>
+                <p>Front-end Developer</p>
+                <Medias />
+              </div>
+            </AboutCard>
+
             <a
               target="_blank"
               href="https://drive.google.com/file/d/1f4wL263wM4zzeECxngxDZUD4X1PYP01P/view?usp=sharing"
             >
-              Download my resume
+              Download Resume
             </a>
-            <p>Check my social medias</p>
-            <Medias />
+
+            <AboutMe>
+              <h2>About me</h2>
+              <p>
+                Hi! My name is <span>Gustavo de Carvalho Rodrigues</span> and
+                I'm a Front-end developer. Passionate about technology and
+                problem solving, making people lives better. I'm in the last
+                semester of my graduation at Fatec Rubens lara in Internet
+                Systems. Currently working with the most modern web development
+                technologies: <strong>ReactJS</strong>,{" "}
+                <strong>React Native</strong> and <strong>Node.js</strong>
+              </p>
+              <p>
+                I'm dynamic, creative, communicative, committed, with team
+                spirit, willingness, always looking for self improvement as
+                developer and human and searching for new challenges.
+              </p>
+            </AboutMe>
+            <Techs>
+              <h2>Technologies</h2>
+              <div>
+                <Tech background={html}>
+                  <p>HTML5</p>
+                </Tech>
+                <Tech background={css}>
+                  <p>CSS3</p>
+                </Tech>
+                <Tech background={javascript}>
+                  <p>JAVASCRIPT</p>
+                </Tech>
+                <Tech background={typescript}>
+                  <p>TYPESCRIPT</p>
+                </Tech>
+                <Tech background={react}>
+                  <p>REACTJS & REACT NATIVE</p>
+                </Tech>
+                <Tech background={styled}>
+                  <p>STYLED COMPONENTS</p>
+                </Tech>
+                <Tech background={redux}>
+                  <p>REDUX</p>
+                </Tech>
+                <Tech background={sass}>
+                  <p>SASS</p>
+                </Tech>
+                <Tech background={git}>
+                  <p>GIT</p>
+                </Tech>
+                <Tech background={node}>
+                  <p>NODEJS</p>
+                </Tech>
+              </div>
+            </Techs>
           </div>
         </Content>
       </Section>
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,43 +1,205 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { darken } from "polished";
+
+const shake = keyframes`
+  0%, 100% {
+    transform: rotate(0deg);
+  }
+
+  20%, 60%{
+    transform: rotate(5deg);
+  }
+  40%, 80%{
+    transform: rotate(-5deg);
+  }
+`;
 
 export const Content = styled.div`
-  width: 100%;
-  height: 100%;
+  padding: 2rem;
+
+  @media (min-width: 1000px) {
+    padding: 4rem;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  > div {
+    perspective: 100rem;
+    display: flex;
+    flex-direction: column;
+
+    max-width: 1024px;
+
+    @media (min-width: 1000px) {
+      display: grid;
+      grid-template-rows: 1fr 20rem;
+      grid-template-columns: 30% auto;
+      grid-column-gap: 3rem;
+      grid-row-gap: 2rem;
+      grid-template-areas:
+        "card about about"
+        "card tech tech"
+        ". download download";
+    }
+
+    > a {
+      grid-area: download;
+
+      color: white;
+      background-color: #53b853;
+
+      margin: 2rem 0;
+      padding: 1rem 2.5rem;
+      border-radius: 0.5rem;
+
+      font-weight: bold;
+      font-size: 1.6rem;
+      text-transform: uppercase;
+      text-decoration: none;
+      text-align: center;
+
+      transition: background-color 0.2s;
+
+      &:hover,
+      &:focus {
+        background-color: ${darken(0.1, "#53b853")};
+      }
+    }
+    h2 {
+      font-size: 2.5rem;
+      margin-bottom: 15px;
+    }
+  }
+`;
+
+export const AboutCard = styled.div`
+  grid-area: card;
+
+  margin: 0 auto;
 
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 45rem;
+
+  height: fit-content;
 
   > div {
     display: flex;
-    align-items: center;
     flex-direction: column;
+    align-items: center;
 
-    background: ${(props) => props.theme.colors.complement};
-    padding: 2rem;
-    border-radius: 0.5rem;
-
-    > a {
-      padding: 1rem 2.5rem;
-      font-size: 1.6rem;
-      color: white;
-      text-decoration: none;
-      background-color: ${(props) => props.theme.colors.secundary};
-      margin: 1rem 0;
-    }
-
-    h2 {
-      font-size: 2.4rem;
+    h1 {
+      font-size: 2.5rem;
+      margin-top: 20px;
     }
 
     p {
-      font-size: 1.5rem;
-    }
+      font-size: 1.6rem;
+      margin: 8px 0;
+      position: relative;
 
-    ul {
-      a {
-        margin-top: 0;
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        margin-top: 10px;
+
+        background-color: ${(props) => props.theme.colors.text};
       }
     }
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  width: 75%;
+  height: 75%;
+  border: solid 0.5rem transparent;
+  border-radius: 50%;
+  position: relative;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -0.5rem;
+    border-radius: inherit;
+    background: ${(props) =>
+      `linear-gradient(-45deg, ${props.theme.colors.primary}, ${props.theme.colors.secundary})`};
+  }
+
+  img {
+    width: 100%;
+    border-radius: 50%;
+  }
+`;
+
+export const AboutMe = styled.div`
+  grid-area: about;
+  margin: 10px 0;
+
+  span {
+    color: ${(props) => props.theme.colors.secundary};
+  }
+
+  p {
+    font-size: 1.8rem;
+    line-height: 1.6;
+
+    & + p {
+      margin-top: 10px;
+    }
+  }
+`;
+
+export const Techs = styled.div`
+  grid-area: tech;
+  margin: 10px 0;
+
+  > div {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 7rem);
+    grid-gap: 1rem;
+  }
+`;
+
+export const Tech = styled.div`
+  width: 7rem;
+  height: 7rem;
+  background-image: ${(props) => `url(${props.background})`};
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover,
+  &:focus {
+    animation: ${shake} 0.3s linear;
+    background-image: none;
+
+    p {
+      opacity: 1;
+    }
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-weight: bold;
+    opacity: 0;
+    transition: opacity 0.2s;
+    text-align: center;
   }
 `;
