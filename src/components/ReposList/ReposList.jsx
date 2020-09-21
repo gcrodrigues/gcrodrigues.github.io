7883485543c1aa40c9ff6aa632c8cd92a00bfbe1
Gustavo Carvalho <gcrodrigues413@gmail.com>
2020-09-21 17:21:55 -0300

    Fetching GH data and stored in local storage

--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -1,24 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { Repo } from "../";
-import api, { GET_PINNED_REPOS } from "../../services/api";
+import { FiLoader } from "react-icons/fi";
 
-import { Container } from "./styles.js";
+import { Repo } from "../";
+import api from "../../services/api";
+
+import { Container, Loading } from "./styles.js";
 
 const ReposList = () => {
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState(() => {
+    const storagedRepos = localStorage.getItem("@Portfolio:repositories");
+    if (storagedRepos) {
+      return JSON.parse(storagedRepos);
+    }
+
+    return [];
+  });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    api.post("", { query: GET_PINNED_REPOS }).then(
-      ({
-        data: {
-          data: { user },
-        },
-      }) => {
-        const res = user.pinnedItems.nodes;
-        setRepos(res);
-      }
-    );
+    if (!!localStorage.getItem("@Portfolio:repositories")) {
+      return;
+    }
+
+    fetchGithubRepos();
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("@Portfolio:repositories", JSON.stringify(repos));
+  }, [repos]);
+
+  const fetchGithubRepos = async () => {
+    setLoading(true);
+    const { data } = await api.get("users/gcrodrigues/repos");
+
+    const languagesData = await Promise.all(
+      data
+        .sort(
+          (a, b) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        )
+        .filter(
+          (repo) =>
+            repo.name !== "gostack-desafio-conceitos-reactjs" &&
+            repo.name !== "gcrodrigues.github.io" &&
+            new Date(repo.created_at).getTime() >= 1584987810000
+        )
+        .map(async (repo) => {
+          const resposta = await api.get(
+            `repos/gcrodrigues/${repo.name}/languages`
+          );
+          repo.languages = resposta.data;
+          return repo;
+        })
+    );
+
+    setRepos(languagesData);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return (
+      <Loading>
+        <FiLoader size={45} />
+      </Loading>
+    );
+  }
 
   return (
     <Container>
@@ -26,9 +72,9 @@ const ReposList = () => {
         <Repo
           key={repo.id}
           name={repo.name}
-          url={repo.url}
-          homepage={repo.homepageUrl}
+          url={repo.html_url}
           description={repo.description}
+          languages={repo.languages}
         />
       ))}
     </Container>
--- src/components/ReposList/styles.js
+++ src/components/ReposList/styles.js
@@ -1,9 +1,35 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform: rotate(360deg);
+  }
+`;
 
 export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1920px;
   padding: 3rem;
   display: grid;
   gap: 2rem;
   grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
-  grid-template-rows: repeat(2, 1fr);
+  grid-template-rows: repeat(auto-fit, 1fr);
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+
+  animation: ${rotate} 1s infinite linear;
+
+  svg {
+    color: ${(props) => props.theme.colors.text};
+  }
 `;
